3721a5c2acdc05845e5d8c8c61258e1b956067ec
auralapse <auralapse@gmail.com>
2017-04-06 12:39:31 -0600

    Added psuedo from tut

--- A5.c
+++ A5.c
@@ -2,7 +2,30 @@
 #include <pthread.h>
 #include <stding.h>
 
-// producer struct
+/*
+P R O D U C E R
+psuedocode from tutorial
+*/
 
-// consumer struct 
+// Acquire lock 
+    // if (elem < queueSize)
+        // Add to queue 
 
+    // else 
+        // pthread_cond_wait(&fullcondition, &lock)
+        // Add to queue 
+    // Elem++ 
+// Release lock
+
+
+/*
+C O N S U M E R 
+psuedocode from tutorial
+*/
+
+// Acquire lock 
+    // Remove an element from queue 
+    // if(elem == queueSize)
+        // Pthread_cond_signal(&fullcondition)
+    // Elem--
+// Release lock
