b680ed4855d94f4a109693fbcd06924909f5ef21
auralapse <auralapse@gmail.com>
2017-04-09 23:24:32 -0600

    Dat loop is added

--- A5.c
+++ A5.c
@@ -38,33 +38,38 @@ With unresolved race condition
 
 void producer(void* threadArg){
 
-    // Get object from queue
-    producerStruct* prod = (producerStruct*) arg;
+    // 10 Producer threads
+    int count = 0;
+    while(count < 10){
+        // Get object from queue
+        producerStruct* prod = (producerStruct*) arg;
 
-    printf("Producer ID is: %i", prod->id);
+        printf("Producer ID is: %i", prod->id);
 
-    // Lock thread  
-    pthread_mutex-lock(prod->lock);
+        // Lock thread  
+        pthread_mutex-lock(prod->lock);
 
-    // If there isn't any elements to add, wait 
-    if(prod->queue->remaining_elements == 0){
-        printf("Remaining elements = 0");
+        // If there isn't any elements to add, wait 
+        if(prod->queue->remaining_elements == 0){
+            printf("Remaining elements = 0");
 
-        // increment queue wait 
-        prod->queue->wait++;
+            // increment queue wait 
+            prod->queue->wait++;
 
-        // wait  -- what's cond1?
-        pthread_cond_wait(&prod->queue->cond)1, prod->lock);
+            // wait  -- what's cond1?
+            pthread_cond_wait(&prod->queue->cond)1, prod->lock);
+        }
+
+        // Add to queue 
+        queue_add(prod->queue, prod->id);
+
+        // Signal to consumer - what's cond2? 
+        pthread_cond_signal(&prod->queue->cond2);
+
+        // Release lock 
+        pthread_mutex_unlock(prod->lock);
+        count++;
     }
-
-    // Add to queue 
-    queue_add(prod->queue, prod->id);
-
-    // Signal to consumer - what's cond2? 
-    pthread_cond_signal(&prod->queue->cond2);
-
-    // Release lock 
-    pthread_mutex_unlock(prod->lock);
 }
 
 
