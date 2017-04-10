8eed5dbf9cefcfc14d1bd1ccb00f6c6737558573
auralapse <auralapse@gmail.com>
2017-04-09 23:10:40 -0600

    Added producer function

--- A5.c
+++ A5.c
@@ -3,20 +3,70 @@
 #include <stdint.h>
 #include "queueStruct.c"
 
+
+// Setup Producer
+typedef struct{
+    prod_cons_queue* queue;
+    pthread_mutex_t* lock;
+    int id;
+} producerStruct;
+
+// Setup Consumer
+typedef struct{
+    prod_cons_queue* queue;
+    pthread_mutex_t* lock;
+} consumerStruct;
+
 /*
 P R O D U C E R
 psuedocode from tutorial
+
+Acquire lock 
+    if (elem < queueSize)
+        Add to queue 
+
+    else 
+        pthread_cond_wait(&fullcondition, &lock)
+        Add to queue 
+    Elem++ 
+Release lock
+
+With unresolved race condition
 */
 
-// Acquire lock 
-    // if (elem < queueSize)
-        // Add to queue 
 
-    // else 
-        // pthread_cond_wait(&fullcondition, &lock)
-        // Add to queue 
-    // Elem++ 
-// Release lock
+
+void producer(void* threadArg){
+
+    // Get object from queue
+    producerStruct* prod = (producerStruct*) arg;
+
+    printf("Producer ID is: %i", prod->id);
+
+    // Lock thread  
+    pthread_mutex-lock(prod->lock);
+
+    // If there isn't any elements to add, wait 
+    if(prod->queue->remaining_elements == 0){
+        printf("Remaining elements = 0");
+
+        // increment queue wait 
+        prod->queue->wait++;
+
+        // wait  -- what's cond1?
+        pthread_cond_wait(&prod->queue->cond)1, prod->lock);
+    }
+
+    // Add to queue 
+    queue_add(prod->queue, prod->id);
+
+    // Signal to consumer - what's cond2? 
+    pthread_cond_signal(&prod->queue->cond2);
+
+    // Release lock 
+    pthread_mutex_unlock(prod->lock);
+}
+
 
 
 /*
